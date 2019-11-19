f51ca35fd671c5b2699f7feb95e18edcd49fc552
Nick <nikolayvaklinov@yahoo.com>
2019-11-19 23:49:21 +0000

    string compression commit

---
+++ Cracking_Coding_Interview/Arrays_and_Strings/String_Comparison6.py
@@ -0,0 +1,44 @@
+"""
+String Compression: Implement a method to perform basic string compression
+using the counts of repeated characters. For example, the string
+aabcccccaaa would become a2b1c5a3. If the "compressed" string would not
+become smaller than the original string, your method should return the
+original string. You can assume the string has only uppercase and
+lowercase letters (a - z).
+"""
+import unittest
+
+
+def string_compression(string):
+    compressed = []
+    counter = 0
+
+    for i in range(len(string)):
+        if i != 0 and string[i] != string[i - 1]:
+            compressed.append(string[i - 1] + str(counter))
+            counter = 0
+        counter += 1
+
+    # add last repeated character
+    compressed.append(string[-1] + str(counter))
+
+    # returns original string if compressed string isn't smaller
+    return min(string, ''.join(compressed), key=len)
+
+
+class Test(unittest.TestCase):
+    """Test Cases"""
+    data = [
+        ('aabcccccaaa', 'a2b1c5a3'),
+        ('abcdef', 'abcdef')
+    ]
+
+    def test_string_compression(self):
+        for [test_string, expected] in self.data:
+            actual = string_compression(test_string)
+            self.assertEqual(actual, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
